1da6f9f38813ddb7ba4d68519e5b66d859b8cb03
Bazyli Zygan <bazyl@novaproject.net>
2013-12-14 15:40:27 +0100

    Initial Commit

---
+++ Cooleaf/NPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NPAppDelegate.h
+//  Cooleaf
+//
+//  Created by Bazyli Zygan on 14.12.2013.
+//  Copyright (c) 2013 Nova Project. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Cooleaf/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Cooleaf
+//
+//  Created by Bazyli Zygan on 14.12.2013.
+//  Copyright (c) 2013 Nova Project. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NPAppDelegate class]));
+    }
+}
