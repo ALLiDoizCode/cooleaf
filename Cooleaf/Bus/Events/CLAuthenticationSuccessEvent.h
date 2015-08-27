a7c17afe4ea00e317e6a586ba86fbe9f64abcc99
ZkHaider <hkhan@oglethorpe.edu>
2015-08-27 14:44:49 -0400

    WIP

---
+++ Cooleaf/Bus/Events/CLAuthenticationSuccessEvent.h
@@ -0,0 +1,13 @@
+//
+//  CLAuthenticationSuccessEvent.h
+//  Cooleaf
+//
+//  Created by Haider Khan on 8/27/15.
+//  Copyright (c) 2015 Nova Project. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CLAuthenticationSuccessEvent : NSObject
+
+@end
---
+++ Cooleaf/Bus/Events/CLAuthenticationSuccessEvent.m
@@ -0,0 +1,13 @@
+//
+//  CLAuthenticationSuccessEvent.m
+//  Cooleaf
+//
+//  Created by Haider Khan on 8/27/15.
+//  Copyright (c) 2015 Nova Project. All rights reserved.
+//
+
+#import "CLAuthenticationSuccessEvent.h"
+
+@implementation CLAuthenticationSuccessEvent
+
+@end
